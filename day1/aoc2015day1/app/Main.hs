0797de254ca85fd2920f7c11b21d9187df67ba9a
terry <terry.cadd@gmail.com>
2026-05-24 23:50:48 +0100

    we can load a file in
    BS.readFile "../input"
    
    figuring out the IO monad slowly

--- day1/aoc2015day1/app/Main.hs
+++ day1/aoc2015day1/app/Main.hs
@@ -5,21 +5,26 @@ module Main(main) where
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
 
+import qualified System.Directory as SD 
+
 main :: IO ()
 main = do
     -- Attempt to read the file
-    input <- BS.readFile "../../input"
+    input <- BS.readFile "../input"
+    input2 <- (BS.drop 1 input)
+    input3 <- (BS.dropEnd 1 input2) ??? 
+    putStrLn $ input3 
 
     -- Verify we got something
     let count = BS.length input
     putStrLn $ "Read " ++ show count ++ " bytes."
 
-    -- Optional: Print the first 50 bytes to visually confirm it's not garbage
-    if count > 0 
-        then do
-            let preview = BSC.unpack (BS.take 50 input)
-            putStrLn $ "Preview: " ++ preview
-        else putStrLn "File is empty or path is wrong."
+    -- -- Optional: Print the first 50 bytes to visually confirm it's not garbage
+    -- if count > 0 
+    --     then do
+    --         let preview = BSC.unpack (BS.take 50 input)
+    --         putStrLn $ "Preview: " ++ preview
+    --     else putStrLn "File is empty or path is wrong."
 
 
 --     Could not load module ‘Data.ByteString’.
