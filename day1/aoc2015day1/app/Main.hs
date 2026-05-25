f512f8147cb96f2a018b2588eb7418dc89030390
terry <terry.cadd@gmail.com>
2026-05-25 10:26:55 +0100

    fixed somerthing

--- day1/aoc2015day1/app/Main.hs
+++ day1/aoc2015day1/app/Main.hs
@@ -1,33 +1,55 @@
-
-
 module Main(main) where
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
 
-import qualified System.Directory as SD 
+import qualified System.Directory as SD
+-- import qualified Data.Char as DC 
+import Data.Char(ord)
+
+
+-- isOpen = \x -> x == ord '(' 
+-- isClose = \x -> x == ord ')'
 
 main :: IO ()
 main = do
-    -- Attempt to read the file
     input <- BS.readFile "../input"
-    input2 <- (BS.drop 1 input)
-    input3 <- (BS.dropEnd 1 input2) ??? 
-    putStrLn $ input3 
+    let input2 = BS.drop 1 input
+    let input3 = BS.dropEnd 1 input2 
+    putStrLn $ BSC.unpack input3
+    putStrLn $ "your input file is " ++ show (BS.length input3) ++ " bytes long"
+    let ups = BS.length (BS.filter (\x -> x == fromIntegral (ord '(')) input3) 
+    let downs = BS.length (BS.filter (\x -> x == fromIntegral (ord ')')) input3)   
+    -- let ups = BS.length (BS.filter (\x -> x == 40) input3) 
+    -- let downs = BS.length (BS.filter (\x -> x == 41) input3)   
+    let level = ups - downs
+    putStrLn $ "the final level santa is " ++ show (level) ++ " and goodnight"
 
-    -- Verify we got something
-    let count = BS.length input
-    putStrLn $ "Read " ++ show count ++ " bytes."
 
-    -- -- Optional: Print the first 50 bytes to visually confirm it's not garbage
-    -- if count > 0 
-    --     then do
-    --         let preview = BSC.unpack (BS.take 50 input)
-    --         putStrLn $ "Preview: " ++ preview
-    --     else putStrLn "File is empty or path is wrong."
+        
 
+{--
+
+readFile :: FilePath -> IO ByteString
+drop :: Int -> ByteString -> ByteString
+dropEnd :: Int -> ByteString -> ByteString
+putStrLn :: String -> IO 
+
+-- input2 <- (BS.drop 1 input)
+-- input3 <- (BS.dropEnd 1 input2)
+-- putStrLn $ BSC.unpack input3
+
+-- -- Verify we got something
+-- let count = BS.length input
+-- putStrLn $ "Read " ++ show count ++ " bytes."
+
+-- -- Optional: Print the first 50 bytes to visually confirm it's not garbage
+-- if count > 0
+--     then do
+--         let preview = BSC.unpack (BS.take 50 input)
+--         putStrLn $ "Preview: " ++ preview
+--     else putStrLn "File is empty or path is wrong."
 
 --     Could not load module ‘Data.ByteString’.
 --     Could not load module ‘Data.ByteString.Char8’.
-
-
+--} 
