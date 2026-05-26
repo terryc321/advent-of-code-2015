e4731667704644b20aa2226230e776530146af34
terry <terry.cadd@gmail.com>
2026-05-26 23:05:31 +0100

    interesting
    i think routines look ok , yet get wrong answer ?
    its all type checked , where did we go wrong ?

--- day5/aoc2015day5/app/Main.hs
+++ day5/aoc2015day5/app/Main.hs
@@ -1,10 +1,9 @@
 
-
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import qualified Data.Text.Encoding as TE
-import qualified Text.Read as TR 
+import qualified Text.Read as TR
 import Data.List (sort)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -32,16 +31,70 @@ readFileLines path = do
 --                        ab . . ab
 -- given a pair at i      |
 -- check forward if 
--- 
+--
 
-solvePart1 s = 123
-               
-solvePart2 s = 456
+
+threeVowelsRec :: Int -> [Char] -> Bool
+threeVowelsRec _ [] = False
+threeVowelsRec n (h : t ) =
+  (n >= 3) || (case h of
+         _ | h `elem` "aeiou" -> threeVowelsRec (n + 1) t
+         _ | otherwise -> threeVowelsRec n t)
+
+threeVowels :: [Char] -> Bool
+threeVowels = threeVowelsRec 0
+
+twiceInRow :: [Char] -> Bool
+twiceInRow [] = False
+twiceInRow [_] = False
+twiceInRow (x : y : t ) = (x == y) || twiceInRow (y : t)
+
+
+avoidPair :: [Char] -> Bool
+avoidPair [_] = True
+avoidPair (x : y : t) =
+    case (x, y) of
+        ('a', 'b') -> False
+        ('c', 'd') -> False
+        ('p', 'q') -> False
+        ('x', 'y') -> False
+        _ -> avoidPair (y : t)
+
+nice :: [Char] -> Bool
+nice s = let a = threeVowels s
+             b = twiceInRow s
+             c = avoidPair s
+         in a && b && c
+
+
+solvePart1 xs = length $ filter nice xs
+
+solvePart2 xs = 456
 
 main :: IO ()
-main = do  input <- readFileLines "../input"
-           let part1 = solvePart1 lines  
-           putStrLn $ "part 1  = " ++ show part1 ++ " and goodnight"           
-           let part2 = solvePart2 lines
+main = do  input <- readFile "../input.txt"
+           let myLines = lines input
+           putStrLn $ "the input was " ++ show myLines ++ " and goodnight"
+           let example1 = nice "ugknbfddgicrmopn"
+           putStrLn $ "example 1  = " ++ show example1 ++ " and goodnight"
+           let example2 = nice "jchzalrnumimnmhp"
+           putStrLn $ "example 2  = " ++ show example2 ++ " and goodnight"
+           let example3 = nice "haegwjzuvuyypxyu"
+           putStrLn $ "example 3  = " ++ show example3 ++ " and goodnight"
+           let example4 = nice "dvszwmarrgswjxmb"
+           putStrLn $ "example 4  = " ++ show example4 ++ " and goodnight"
+           let part1 = solvePart1 myLines
+           putStrLn $ "part 1  = " ++ show part1 ++ " and goodnight"
+           let part2 = solvePart2 myLines
            putStrLn $ "part 2 = " ++ show part2 ++ " and goodnight"
-        
+
+
+{--
+example 1  = True and goodnight
+example 2  = False and goodnight
+example 3  = False and goodnight
+example 4  = False and goodnight
+part 1  = 227 and goodnight
+
+
+--}
