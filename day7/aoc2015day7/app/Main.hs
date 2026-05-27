7596c9881c2ddf5c0cb3fb1c122b55ad8b12c72a
terry <terry.cadd@gmail.com>
2026-05-28 00:50:38 +0100

    traced bug
    solved day7 , performance ok to be  honest

--- day7/aoc2015day7/app/Main.hs
+++ day7/aoc2015day7/app/Main.hs
@@ -1,6 +1,8 @@
 
 module Main (main) where
 
+import Debug.Trace  -- debugging 
+
 import Data.List(isInfixOf)
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -45,7 +47,8 @@ myparse str =
      in if (w !! 0) == "NOT" then (sk 3 , Not (mkW 1))
         else if (w !! 1) == "LSHIFT" then (sk 4 , Lshift (mkW 0) (mkL 2))
         else if (w !! 1) == "RSHIFT" then (sk 4 , Rshift (mkW 0) (mkL 2))
-        else if (w !! 1) == "AND"    then (sk 4 ,    And (mkW 0) (mkW 2))
+        else if (w !! 1) == "AND"    then if isDigit ((w !! 0) !! 0) then  (sk 4 , And (Literal (read (w !! 0) :: Word16)) (mkW 2))
+                                          else (sk 4 , And (mkW 0) (mkW 2))                            
         else if (w !! 1) == "OR"     then (sk 4 ,    Or  (mkW 0) (mkW 2))
         else if (w !! 1) == "->"     then
                if isDigit ((w !! 0) !! 0) then (sk 2 , Direct (Literal (read (w !! 0) :: Word16)))
@@ -64,11 +67,13 @@ type ValueMap = Map String Word16
 -- we do redundant work perhaps by not shortcutting as soon as symbol is resolved
 -- maybe CPS could help 
 resolveTarget :: String -> [(String , Operation)] -> ValueMap -> Word16
-resolveTarget t mp vm = let vm2 = resolve mp vm
-                        in case Map.lookup t vm2 of
-                             Just x -> x
-                             _ -> resolveTarget t mp vm2 
-                             
+resolveTarget t mp vm =  
+   let vm2 = resolve mp vm
+   in case Map.lookup t vm2 of
+        Just x -> x
+        _ -> resolveTarget t mp vm2
+
+             
 
 
 -- list of tuples String , Operation  and Map String Operation 
@@ -83,67 +88,86 @@ resolve (h : t ) vm = let (sym , op) = h
 tryResolve :: String -> Operation -> ValueMap -> ValueMap 
 tryResolve sym op vm =
   case op of
-    And (Wire s1) (Wire s2) -> andResolve sym s1 s2 vm 
-    Or (Wire s1) (Wire s2) -> orResolve sym s1 s2 vm 
-    Lshift (Wire s1) i2 -> lshiftResolve sym s1 i2 vm 
-    Rshift (Wire s1) i2 -> rshiftResolve sym s1 i2 vm    
-    Not (Wire s1) -> notResolve sym s1 vm 
-    Direct (Wire s1) -> directwResolve sym s1 vm 
-    Direct (Literal w1) -> directlResolve sym w1 vm
-    _ -> vm -- else give up 
+    And (Wire s1) (Wire s2) -> andResolve sym op s1 s2 vm
+    And (Literal w1) (Wire s2) -> andwResolve sym op w1 s2 vm 
+    Or (Wire s1) (Wire s2) -> orResolve sym op s1 s2 vm 
+    Lshift (Wire s1) i2 -> lshiftResolve sym op s1 i2 vm 
+    Rshift (Wire s1) i2 -> rshiftResolve sym op s1 i2 vm    
+    Not (Wire s1) -> notResolve sym op s1 vm 
+    Direct (Wire s1) -> directwResolve sym op s1 vm 
+    Direct (Literal w1) -> directlResolve sym op w1 vm
+    _ -> trace ("tryResolve shucks " ++ show op ++ "\n") $  vm -- else give up 
+
+andwResolve :: String -> Operation -> Word16 -> String -> ValueMap -> ValueMap  
+andwResolve sym op w1 s2 vm =
+  let v2 = Map.lookup s2 vm
+  in case v2 of
+    Just y -> let val =  (w1 .&. y)
+                         in trace ("AND " ++ show op ++ " <== " ++ show val ++ "\n") $ Map.insert sym val vm
+    Nothing -> vm
+    
 
 
 -- if string s1 is resolved then string sym is resolved also
 -- simple pass through puts key into value map 
-directwResolve :: String -> String -> ValueMap -> ValueMap 
-directwResolve sym s1 vm =
+directwResolve :: String -> Operation -> String -> ValueMap -> ValueMap 
+directwResolve sym op s1 vm =
     let v1 = Map.lookup s1 vm
     in case v1 of
-      Just x  -> Map.insert sym x vm
+      Just x  -> trace ("directw " ++ show op ++ " <== " ++ show x ++ "\n") $ Map.insert sym x vm
       _ -> vm 
 
 --- easiest no resolving to do literally given the answer
 --- a direct wire 
-directlResolve :: String -> Word16 -> ValueMap -> ValueMap 
-directlResolve sym w1 vm = Map.insert sym w1 vm 
+directlResolve :: String -> Operation -> Word16 -> ValueMap -> ValueMap 
+directlResolve sym op w1 vm = trace ("directw " ++ show op ++ " <== " ++ show w1 ++ "\n" ) $ Map.insert sym w1 vm 
 
   
-andResolve :: String -> String -> String -> ValueMap -> ValueMap  
-andResolve sym s1 s2 vm =
+andResolve :: String -> Operation -> String -> String -> ValueMap -> ValueMap  
+andResolve sym op s1 s2 vm =
   let v1 = Map.lookup s1 vm
       v2 = Map.lookup s2 vm
   in case (v1,v2) of
-    (Just x , Just y) -> Map.insert sym (x .&. y) vm
-    _ -> vm 
+    (Just x , Just y) -> let val =  (x .&. y)
+                         in trace ("AND " ++ show op ++ " <== " ++ show val ++ "\n") $ Map.insert sym val vm
+    (Just x , Nothing) ->  trace ("AND " ++ show op ++ " only " ++ show s1 ++ " has value " ++ show v1 ++ "\n") $ vm
+    (Nothing ,Just x) ->  trace ("AND " ++ show op ++ " only " ++ show s2 ++ " has value " ++ show v2 ++ "\n") $ vm
+    (Nothing ,Nothing) -> vm 
+    _ -> trace ("AND " ++ show op ++ " wacko \n") $ vm 
+
       
-orResolve :: String -> String -> String -> ValueMap -> ValueMap  
-orResolve sym s1 s2 vm =
+orResolve :: String -> Operation -> String -> String -> ValueMap -> ValueMap  
+orResolve sym op s1 s2 vm =
   let v1 = Map.lookup s1 vm
       v2 = Map.lookup s2 vm
   in case (v1,v2) of
-    (Just x , Just y) -> Map.insert sym (x .|. y) vm
+    (Just x , Just y) -> let val =  (x .|. y)
+                         in trace ("OR " ++ show op ++ " <== " ++ show val ++ "\n") $ Map.insert sym val vm
     _ -> vm 
 
-notResolve :: String -> String -> ValueMap -> ValueMap  
-notResolve sym s1 vm =
+notResolve :: String -> Operation -> String -> ValueMap -> ValueMap  
+notResolve sym op s1 vm =
   let v1 = Map.lookup s1 vm
   in case v1 of
-    Just x  -> Map.insert sym (complement x) vm
+    Just x  -> let val = complement x
+               in trace ("NOT " ++ show op ++ " <== " ++ show val ++ "\n") $ Map.insert sym (complement x) vm
     _ -> vm 
       
-lshiftResolve :: String -> String -> Int -> ValueMap -> ValueMap  
-lshiftResolve sym s1 i2 vm =
+lshiftResolve :: String -> Operation -> String -> Int -> ValueMap -> ValueMap  
+lshiftResolve sym op s1 i2 vm =
   let v1 = Map.lookup s1 vm
   in case v1 of
-    Just x  -> Map.insert sym (shiftL x i2) vm
+    Just x  -> let val = (shiftL x i2)
+               in trace ("LShift " ++ show op ++ " <== " ++ show val ++ "\n") $ Map.insert sym val vm
     _ -> vm 
 
 
-rshiftResolve :: String -> String -> Int -> ValueMap -> ValueMap  
-rshiftResolve sym s1 i2 vm =
+rshiftResolve :: String -> Operation -> String -> Int -> ValueMap -> ValueMap  
+rshiftResolve sym op s1 i2 vm =
   let v1 = Map.lookup s1 vm
   in case v1 of
-    Just x  -> Map.insert sym (shiftR x i2) vm
+    Just x  -> let val = (shiftR x i2)
+               in trace ("RShift " ++ show op ++ " <== " ++ show val ++ "\n") $ Map.insert sym val vm
     _ -> vm 
       
 
@@ -170,7 +194,9 @@ main = do input <- readFile "../input.txt"
                                                             in a == "") mp)
           putStrLn $ "value map => " ++ show vm
           let out = resolveTarget "a" mp vm
-          putStrLn $ "wire on 'a' resolved to  => " ++ show out
+          putStrLn $ "wire on 'a' resolved to  => " ++ show out ++ "\n"
+
+          
           
             
 
